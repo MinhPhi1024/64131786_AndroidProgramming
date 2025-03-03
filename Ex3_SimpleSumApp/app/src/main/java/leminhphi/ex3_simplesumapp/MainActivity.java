d0a73d4e6523aa6c636a6b4456de1937ae6dadcc
LeMinhPhi <humor.mp.1024@gmail.com>
2025-03-03 08:27:02 +0700

    update

--- Ex3_SimpleSumApp/app/src/main/java/leminhphi/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/leminhphi/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package leminhphi.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //Gắn layout tương ứng cho file này
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    //Đây là bộ lắng nghe và bộ xử lý sự kiện onlick Tính tổng
+    public void XuLyCong(View view) {
+        //tìm và tham chiếu đến file XML mapping sang Java
+        EditText edtA = findViewById(R.id.edtA);
+        EditText edtB = findViewById(R.id.edtB);
+        EditText edtKQ = findViewById(R.id.edtKQ);
+
+
+
     }
 }
