2aa41635be74d011b2a0bc119dbc59bd31ba89ba
LeMinhPhi <humor.mp.1024@gmail.com>
2025-03-05 11:38:04 +0700

    update

--- Ex5_AddSubMulDiv_Anynomous/app/src/main/java/leminhphi/ex5_addsubmuldiv_anynomous/MainActivity.java
+++ Ex5_AddSubMulDiv_Anynomous/app/src/main/java/leminhphi/ex5_addsubmuldiv_anynomous/MainActivity.java
@@ -1,6 +1,9 @@
 package leminhphi.ex5_addsubmuldiv_anynomous;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,52 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edt1, edt2, edtKQ;
+    Button btnCong, btnTru, btnNhan, btnChia;
+    void TimDieuKhien(){
+        edt1 = (EditText) findViewById(R.id.edt1);
+        edt2 = (EditText) findViewById(R.id.edt2);
+        edtKQ = (EditText) findViewById(R.id.edtKQ);
+        btnCong = (Button) findViewById(R.id.btnCong);
+        btnTru = (Button) findViewById(R.id.btnTru);
+        btnNhan = (Button) findViewById(R.id.btnNhan);
+        btnChia = (Button) findViewById(R.id.btnChia);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+        TimDieuKhien();
+        //Gắn bộ lắng nghe sự kiện và code xử lý cho từng btn
+        //Cách viết tường mình
+        View.OnClickListener boLangNgheCong = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý code
+            }
+        };
+        btnCong.setOnClickListener(boLangNgheCong);
+        //Cách viết ẩn danh
+        btnTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý code
+            }
+        };
+        btnNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý code
+            }
+        };
+        btnChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Xử lý code
+            }
+        };
+
+    };
 }
