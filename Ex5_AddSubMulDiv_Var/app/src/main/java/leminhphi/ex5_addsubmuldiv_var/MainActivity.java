80bc42a10417a28899b92e51714eaeef1792be67
LeMinhPhi <humor.mp.1024@gmail.com>
2025-03-05 12:21:37 +0700

    update

---
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/leminhphi/ex5_addsubmuldiv_var/MainActivity.java
@@ -0,0 +1,26 @@
+package leminhphi.ex5_addsubmuldiv_var;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt1, edt2;
+    Button btnCong, btnTru, btnNhan, btnChia;
+    TextView txtKQ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
