30737384a660fd02aaf043452aed66ed564ceb3d
LeMinhPhi <humor.mp.1024@gmail.com>
2025-03-12 12:00:34 +0700

    LVBank

---
+++ LVBank/app/src/main/java/ntu/leminhphi/example/lvbank/MainActivity.java
@@ -0,0 +1,41 @@
+package ntu.leminhphi.example.lvbank;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listViewBank;
+    ArrayList<String> dsNganHang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listViewBank.findViewById(R.id.ListBank);
+        //B1.chuẩn bị dữ liệu
+        dsNganHang = new ArrayList<String>();
+        dsNganHang.add("Vietcombank");
+        dsNganHang.add("Techcombank");
+        dsNganHang.add("Vietcombank");
+        dsNganHang.add("BIDV");
+        dsNganHang.add("VietinBank");
+        dsNganHang.add("Agribank");
+        dsNganHang.add("ACB");
+        dsNganHang.add("MB Bank");
+        dsNganHang.add("Sacombank");
+        dsNganHang.add("VPBank");
+        dsNganHang.add("SHB");
+        dsNganHang.add("HDBank");
+        //B2.tạo adapter
+        
+    }
+}
