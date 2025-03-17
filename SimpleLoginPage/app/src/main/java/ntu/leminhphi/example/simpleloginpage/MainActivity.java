393bc4f32e7d65b586970116c14c0dd6451a174e
LeMinhPhi <humor.mp.1024@gmail.com>
2025-03-17 14:43:17 +0700

    update

--- SimpleLoginPage/app/src/main/java/ntu/leminhphi/example/simpleloginpage/MainActivity.java
+++ SimpleLoginPage/app/src/main/java/ntu/leminhphi/example/simpleloginpage/MainActivity.java
@@ -1,6 +1,10 @@
 package ntu.leminhphi.example.simpleloginpage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button loginbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginbutton = findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if  (username.getText().toString().equals("admin")
+                        && password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
