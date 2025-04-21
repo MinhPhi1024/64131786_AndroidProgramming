595af4e2a0e61b68053b26acd8052c35936ef47b
LeMinhPhi <humor.mp.1024@gmail.com>
2025-04-21 09:55:10 +0700

    FragmentEx_Addynamic

---
+++ FragmentEx_AddDynamic/app/src/main/java/ntu/leminhphi/example/fragmentex_adddynamic/MainActivity.java
@@ -0,0 +1,28 @@
+package ntu.leminhphi.example.fragmentex_adddynamic;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+
+                .add(R.id.contentFrament, new ContentFragment())
+                .add(R.id.footerFragment, new FooterFragment())
+
+                .commit();
+    }
+}
