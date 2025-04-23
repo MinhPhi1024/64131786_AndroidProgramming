7b533e63b1eeb8f83c28971bd3ca2a451131bef9
LeMinhPhi <humor.mp.1024@gmail.com>
2025-04-23 15:08:25 +0700

    update

--- Ex_BottomNavigation_bar/app/src/main/java/ntu/leminhphi/example/ex_bottomnavigation_bar/MainActivity.java
+++ Ex_BottomNavigation_bar/app/src/main/java/ntu/leminhphi/example/ex_bottomnavigation_bar/MainActivity.java
@@ -1,24 +1,49 @@
 package ntu.leminhphi.example.ex_bottomnavigation_bar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //tìm điểu khiển bottom_nav
+        bottomNavView = findViewById(R.id.bottom_nav);
+        //lắng nghe
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemID = item.getItemId();
+                if (mnuItemID == R.id.mnu_home) {
+                    //bằng thì thay fragment
+
+                }
+                else if (mnuItemID == R.id.mnu_search) {
+                    Toast.makeText(MainActivity.this, "Thay SEARCH", Toast.LENGTH_SHORT).show();
+                }
+                else if (mnuItemID == R.id.mnu_profile) {
+                    Toast.makeText(MainActivity.this, "Thay PROFILE", Toast.LENGTH_SHORT).show();
+                }
+                else return false;
+
+                return true;
+            }
         });
     }
 }
