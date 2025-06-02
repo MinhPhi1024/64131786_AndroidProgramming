f857134f10bac88158e1866a5059798c2cb35c7d
LeMinhPhi <humor.mp.1024@gmail.com>
2025-06-02 16:51:11 +0700

    Math Quiz

---
+++ MathQuiz/app/src/main/java/ntu/leminhphi/example/eduquiz/Login.java
@@ -0,0 +1,100 @@
+package ntu.leminhphi.example.eduquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Login extends AppCompatActivity {
+    EditText edtemail, edtpassword;
+    Button btnDangnhap;
+    TextView tvDangky;
+    ProgressBar progressLogin;
+    FirebaseAuth mAuth;
+    FirebaseFirestore mstore;
+
+    public void TimKiem()
+    {
+        edtemail = (EditText) findViewById(R.id.edtemail);
+        edtpassword = (EditText) findViewById(R.id.edtpassword);
+        btnDangnhap = (Button) findViewById(R.id.btnDangnhap);
+        tvDangky = (TextView) findViewById(R.id.tvDangky);
+        progressLogin = (ProgressBar) findViewById(R.id.progressLogin);
+        mAuth = FirebaseAuth.getInstance();
+        mstore = FirebaseFirestore.getInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        TimKiem();
+
+        tvDangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = edtemail.getText().toString();
+                String mPassword = edtpassword.getText().toString();
+
+                if(mEmail.equals("admin") && mPassword.equals("admin")){
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                    finish();
+                }
+                if(TextUtils.isEmpty(mEmail)){
+                    edtemail.setError("Vui lòng nhập email");
+                    return;
+                }
+                if(TextUtils.isEmpty(mPassword)){
+                    edtpassword.setError("Vui lòng nhập mật khẩu");
+                    return;
+                }
+
+                progressLogin.setVisibility(View.VISIBLE);
+
+                mAuth.signInWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, MainActivity.class));
+                            finishAffinity();
+                        }else{
+                            Toast.makeText(Login.this, "Email hoặc mật khẩu không đúng", Toast.LENGTH_SHORT).show();
+                            progressLogin.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+}
---
+++ MathQuiz/app/src/main/java/ntu/leminhphi/example/eduquiz/SplashScreen.java
@@ -0,0 +1,47 @@
+package ntu.leminhphi.example.eduquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    TextView motaApp, devApp;
+    ImageView logoApp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        motaApp = findViewById(R.id.motaApp);
+        devApp = findViewById(R.id.devApp);
+        logoApp = findViewById(R.id.logoApp);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //set animation
+        logoApp.setAnimation(bottomAnim);
+        motaApp.setAnimation(topAnim);
+        devApp.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
